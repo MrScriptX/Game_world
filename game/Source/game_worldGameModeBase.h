0e48b663f7d3786fa1c8d1c54da2257f41c49ef3
MrScriptX <scriptx001@gmail.com>
2017-11-25 13:23:51 +0100

    Add files via upload

---
+++ game/Source/game_worldGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "game_worldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_WORLD_API Agame_worldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
